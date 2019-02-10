b179405244371e532465c0248702f07eb8dc87d2
Daniel Ignat <dignite@users.noreply.github.com>
2019-02-10 21:05:53 +0100

    More info page

--- build.js
+++ build.js
@@ -8,38 +8,34 @@ fs.mkdirSync("docs");
 
 copydir.sync("src", "docs");
 
-critical
-  .generate({
-    // Inline the generated critical-path CSS
-    // - true generates HTML
-    // - false generates CSS
-    inline: true,
+critical.generate({
+  // Inline the generated critical-path CSS
+  // - true generates HTML
+  // - false generates CSS
+  inline: true,
 
-    // Your base directory
-    base: "docs/",
+  // Your base directory
+  base: "docs/",
 
-    // HTML source file
-    src: "index.html",
+  // HTML source file
+  src: "index.html",
 
-    // Viewport width
-    width: 1300,
+  // Viewport width
+  width: 1300,
 
-    // Viewport height
-    height: 900,
+  // Viewport height
+  height: 900,
 
-    // Target for final HTML output.
-    // use some CSS file when the inline option is not set
-    dest: "index.html",
+  // Target for final HTML output.
+  // use some CSS file when the inline option is not set
+  dest: "index.html",
 
-    // Minify critical-path CSS when inlining
-    minify: true,
+  // Minify critical-path CSS when inlining
+  minify: true,
 
-    // Extract inlined styles from referenced stylesheets
-    extract: true,
+  // Extract inlined styles from referenced stylesheets
+  extract: true,
 
-    // Complete Timeout for Operation
-    timeout: 30000
-  })
-  .then(function(output) {
-    fs.unlinkSync("docs/main.css");
-  });
+  // Complete Timeout for Operation
+  timeout: 30000
+});
